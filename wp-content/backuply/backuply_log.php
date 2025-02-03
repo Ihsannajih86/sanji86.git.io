c04bddeb978070300964e2093a5543c81cbcb25e
sanji <amanyalsrogowy@gmail.com>
2025-02-03 20:03:55 +0700

    Add files via upload

---
+++ wp-content/backuply/backuply_log.php
@@ -0,0 +1,63 @@
+<?php exit();?>
+Initializing...|info|13
+<?php exit();?>
+Creating a job to start Backup|info|17
+Starting to Backup Database|info|20
+Creating softsql|working|23
+Adding (L1) : wp1c_actionscheduler_actions table|working|50
+Adding (L1) : wp1c_actionscheduler_claims table|working|50
+Adding (L1) : wp1c_actionscheduler_groups table|working|50
+Adding (L1) : wp1c_actionscheduler_logs table|working|50
+Adding (L1) : wp1c_commentmeta table|working|50
+Adding (L1) : wp1c_comments table|working|50
+Adding (L1) : wp1c_e_events table|working|50
+Adding (L1) : wp1c_e_submissions table|working|50
+Adding (L1) : wp1c_e_submissions_actions_log table|working|50
+Adding (L1) : wp1c_e_submissions_values table|working|50
+Adding (L1) : wp1c_links table|working|50
+Adding (L1) : wp1c_options table|working|50
+Adding (L1) : wp1c_postmeta table|working|50
+Adding (L1) : wp1c_posts table|working|50
+Adding (L1) : wp1c_srfm_entries table|working|50
+Adding (L1) : wp1c_termmeta table|working|50
+Adding (L1) : wp1c_terms table|working|50
+Adding (L1) : wp1c_term_relationships table|working|50
+Adding (L1) : wp1c_term_taxonomy table|working|50
+Adding (L1) : wp1c_usermeta table|working|50
+Adding (L1) : wp1c_users table|working|50
+Creation of SQL dump completed|working|24
+Backing up your Wordpress Install|info|39
+Adding to the archive|info|60
+Adding files from directory (L1) : /home/portofo1/mybiografi.site/wp-admin|adding|65
+(L1) : 500 files have been added to the backup|adding|65
+Adding files from directory (L1) : /home/portofo1/mybiografi.site/wp-content|adding|65
+Adding files from directory (L1) : /home/portofo1/mybiografi.site/wp-content/themes|adding|65
+(L1) : 1000 files have been added to the backup|adding|65
+(L1) : 1500 files have been added to the backup|adding|65
+(L1) : 2000 files have been added to the backup|adding|65
+Adding files from directory (L1) : /home/portofo1/mybiografi.site/wp-content/plugins|adding|65
+(L1) : 2500 files have been added to the backup|adding|65
+(L1) : 3000 files have been added to the backup|adding|65
+(L1) : 3500 files have been added to the backup|adding|65
+(L1) : 4000 files have been added to the backup|adding|65
+(L1) : 4500 files have been added to the backup|adding|65
+(L1) : 5000 files have been added to the backup|adding|65
+(L1) : 5500 files have been added to the backup|adding|65
+(L1) : 6000 files have been added to the backup|adding|65
+(L1) : 6500 files have been added to the backup|adding|65
+(L1) : 7000 files have been added to the backup|adding|65
+(L1) : 7500 files have been added to the backup|adding|65
+(L1) : 8000 files have been added to the backup|adding|65
+(L1) : 8500 files have been added to the backup|adding|65
+(L1) : 9000 files have been added to the backup|adding|65
+(L1) : 9500 files have been added to the backup|adding|65
+(L1) : 10000 files have been added to the backup|adding|65
+Adding files from directory (L1) : /home/portofo1/mybiografi.site/wp-includes|adding|65
+(L1) : 10500 files have been added to the backup|adding|65
+(L1) : 11000 files have been added to the backup|adding|65
+(L1) : 11500 files have been added to the backup|adding|65
+(L1) : 12000 files have been added to the backup|adding|65
+(L1) : 12500 files have been added to the backup|adding|65
+Total files added to the backup are 12986|working|0
+Archive created with a file size of 116.03 MB|info|100
+Backup Successfully Completed|success|100
